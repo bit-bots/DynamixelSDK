93e8b9524de541c141fb826ab603a4162c5bf3d3
Marc Bestmann <bestmann@informatik.uni-hamburg.de>
2018-04-22 23:25:59 +0200

    added compiled file of SDK for linux64 and changed python interface to make the SDK work out of the box

--- python/dynamixel_functions_py/dynamixel_functions.py
+++ python/dynamixel_functions_py/dynamixel_functions.py
@@ -21,10 +21,13 @@
 
 import ctypes
 from ctypes import cdll
-dxl_lib = cdll.LoadLibrary("../../c/build/win32/output/dxl_x86_c.dll")  # for windows 32bit
+#dxl_lib = cdll.LoadLibrary("../../c/build/win32/output/dxl_x86_c.dll")  # for windows 32bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/win64/output/dxl_x64_c.dll")  # for windows 64bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/linux32/libdxl_x86_c.so")     # for linux 32bit
-# dxl_lib = cdll.LoadLibrary("../../c/build/linux64/libdxl_x64_c.so")     # for linux 64bit
+from rospkg import RosPack
+rp = RosPack()
+path = rp.get_path('dynamixel_sdk') + "/.."
+dxl_lib = cdll.LoadLibrary(path + "/c/build/linux64/libdxl_x64_c.so")     # for linux 64bit
 # dxl_lib = cdll.LoadLibrary("../../c/build/linux_sbc/libdxl_sbc_c.so")   # for SBC linux
 # dxl_lib = cdll.LoadLibrary("../../c/build/mac/libdxl_mac_c.dylib")      # for Mac OS
 
